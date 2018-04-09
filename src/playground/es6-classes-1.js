8b928a0e07ebdce484f9036324dd7c14a8c9cb96
Corey <corey.sader@gmail.com>
2018-04-09 14:25:52 -0500

    es6 classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -19,11 +19,42 @@ class Person {
     }
 }
 
-// creates an instance of the Person class called me
-const me = new Person('Corey Sader', 43);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age); // this just passes the data through to the parent class (Person)
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description
+    }
+}
 
-const other = new Person();
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if(this.homeLocation){
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+
+        return greeting;
+    }
+}
+
+// creates an instance of the Person class called me
+const me = new Traveler('Corey Sader', 43, 'Prior Lake, MN');
+console.log(me.getGreeting());
+
+const other = new Traveler();
 console.log(other.getGreeting());
-console.log(other.getDescription());
