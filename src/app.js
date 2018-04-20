dc687b68b6805bff3fea67f6df9dd47347c279bf
Corey <corey.sader@gmail.com>
2018-04-20 08:53:27 -0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,33 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+//import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-// OLD WAY
-// class OldSyntax {
-//     constructor() {
-//         this.name = 'Mike';
-//     }
-//     getGreeting() {
-//         return `Hi. My name is ${this.name}.`;
-//     }
-// }
-
-// const oldSyntax = new OldSyntax();
-// console.log(oldSyntax.getGreeting());
-
-
-
-//NEW WAY (no need to define a constructor funtion)
-// class NewSyntax {
-//     name = 'Jen';
-//     getGreeting = () => {
-//         return `Hi. My name is ${this.name}.`;
-//     }
-// }
-// const newSyntax = new NewSyntax();
-// const newGetGreeting = newSyntax.getGreeting;
-// console.log(newGetGreeting());
+ReactDOM.render(<p>This is my Expensify App boilerplate.</p>, document.getElementById('app'));
