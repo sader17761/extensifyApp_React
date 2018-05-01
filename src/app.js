329d0ddff3f0e51cf30f63bde09f470cfff6d53e
Corey <corey.sader@gmail.com>
2018-05-01 14:45:07 -0500

    sorting by date and amount

--- src/app.js
+++ src/app.js
@@ -24,9 +24,10 @@ store.subscribe(() => {
     console.log('Visible Expenses: ', visibleExpenses);
 });
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 23795, note: '1st quarter 2018 water bill', createdAt: 555000}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 9267, note: 'April 2018 gas bill', createdAt: 26500}));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(addExpense({description: 'Water Bill', amount: 4500, note: '1st quarter 2018 water bill', createdAt: 0}));
+store.dispatch(addExpense({description: 'Gas Bill', amount: 0, note: 'April 2018 gas bill', createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 109500, note: 'April bill', createdAt: 0}));
+//store.dispatch(setTextFilter('bill'));
 
 // NOT NEEDED FOR OUR FINAL CODE...
 // this is just an example using time.
